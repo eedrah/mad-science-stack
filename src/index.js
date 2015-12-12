ecd63b6c3ec81b18a277b24c47eca80ded3d8b92
Eedrah <eedrah@users.noreply.github.com>
2015-12-12 19:09:16 +1300

    implement virtual dom

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-console.log('asdf')
-debugger
+var h = require('virtual-dom/h')
+var diff = require('virtual-dom/diff')
+var patch = require('virtual-dom/patch')
+var createElement = require('virtual-dom/create-element')
+
+var root = require('./components/root')
+
+var tree = root('Hello world!')
+var rootNode = createElement(tree)
+document.body.appendChild(rootNode)
